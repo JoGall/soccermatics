6ce515d354ce636059c5b651ad5192da2d192a05
jogall <joedgallagher@gmail.com>
2017-10-01 15:06:53 +0100

    added argument for pitch line colour in soccerSpoke(), soccerPitchBG(), and soccerPitchFG()

--- R/soccerPitchBG.R
+++ R/soccerPitchBG.R
@@ -8,6 +8,7 @@ NULL
 #' 
 #' @param lengthPitch,widthPitch numeric, length and width of pitch in metres.
 #' @param grass if TRUE, draws pitch background in green and lines in white. If FALSE, draws pitch background in white and lines in black.
+#' @param line_col colour of pitch lines
 #' @return a ggplot object
 #' @examples
 #' # get x,y-coords of player #8 during first 10 minutes
@@ -19,14 +20,13 @@ NULL
 #' 
 #' @seealso \code{\link{soccerPitchFG}} for drawing a soccer pitch as foreground over an existing ggplot object
 #' @export
-soccerPitchBG <- function(lengthPitch = 105, widthPitch = 68, grass = FALSE) {
+soccerPitchBG <- function(lengthPitch = 105, widthPitch = 68, grass = FALSE, line_col = "black") {
   # set draw colours
   if(grass) {
     bg_col = "#008000"
-    fg_col = "white"
+    line_col = "white"
   } else {
     bg_col = "white"
-    fg_col = "black"
   }
   
   #draw
@@ -34,28 +34,28 @@ soccerPitchBG <- function(lengthPitch = 105, widthPitch = 68, grass = FALSE) {
     # pitch
     geom_rect(aes(xmin = -4, xmax = lengthPitch + 4, ymin = -4, ymax = widthPitch + 4), fill = bg_col) +
     # outer lines
-    geom_rect(aes(xmin = 0, xmax = lengthPitch, ymin = 0, ymax = widthPitch), fill = bg_col, col = fg_col, lwd = 1.2) +
+    geom_rect(aes(xmin = 0, xmax = lengthPitch, ymin = 0, ymax = widthPitch), fill = bg_col, col = line_col, lwd = 1.2) +
     # centre circle
-    geom_circle(aes(x0 = lengthPitch / 2, y0 = widthPitch / 2, r = 9.15), fill = bg_col, col = fg_col, lwd = 1.2) +
+    geom_circle(aes(x0 = lengthPitch / 2, y0 = widthPitch / 2, r = 9.15), fill = bg_col, col = line_col, lwd = 1.2) +
     # kick off spot
-    geom_circle(aes(x0 = lengthPitch / 2, y0 = widthPitch / 2, r = 0.5), fill = fg_col, col = fg_col, lwd = 1.2) +
+    geom_circle(aes(x0 = lengthPitch / 2, y0 = widthPitch / 2, r = 0.5), fill = line_col, col = line_col, lwd = 1.2) +
     # halfway line
-    geom_segment(aes(x = lengthPitch / 2, y = 0, xend = lengthPitch / 2, yend = widthPitch), col = fg_col, lwd = 1.2) +
+    geom_segment(aes(x = lengthPitch / 2, y = 0, xend = lengthPitch / 2, yend = widthPitch), col = line_col, lwd = 1.2) +
     # penalty arcs
-    geom_arc(aes(x0= 11, y0 = widthPitch / 2, r = 9.15, start = 0.65, end = 2.49), col= fg_col, lwd = 1.2) +
-    geom_arc(aes(x0 = lengthPitch - 11, y0 = widthPitch / 2, r = 9.15, start = 3.79, end = 5.63), col= fg_col, lwd = 1.2) +
+    geom_arc(aes(x0= 11, y0 = widthPitch / 2, r = 9.15, start = 0.65, end = 2.49), col= line_col, lwd = 1.2) +
+    geom_arc(aes(x0 = lengthPitch - 11, y0 = widthPitch / 2, r = 9.15, start = 3.79, end = 5.63), col= line_col, lwd = 1.2) +
     # penalty areas
-    geom_rect(aes(xmin = 0, xmax = 16.5, ymin = widthPitch / 2 - (40.3 / 2), ymax = widthPitch / 2 + (40.3 / 2)), fill = bg_col, col = fg_col, lwd = 1.2) +
-    geom_rect(aes(xmin = lengthPitch - 16.5, xmax = lengthPitch, ymin = widthPitch / 2 - (40.3 / 2), ymax = widthPitch / 2 + (40.3 / 2)), fill = bg_col, col = fg_col, lwd = 1.2) +
+    geom_rect(aes(xmin = 0, xmax = 16.5, ymin = widthPitch / 2 - (40.3 / 2), ymax = widthPitch / 2 + (40.3 / 2)), fill = bg_col, col = line_col, lwd = 1.2) +
+    geom_rect(aes(xmin = lengthPitch - 16.5, xmax = lengthPitch, ymin = widthPitch / 2 - (40.3 / 2), ymax = widthPitch / 2 + (40.3 / 2)), fill = bg_col, col = line_col, lwd = 1.2) +
     # penalty spots
-    geom_circle(aes(x0 = 11, y0 = widthPitch / 2, r = 0.5), fill = fg_col, col = fg_col, lwd = 1.2) +
-    geom_circle(aes(x0 = lengthPitch - 11, y0 = widthPitch / 2, r = 0.5), fill = fg_col, col = fg_col, lwd = 1.2) +
+    geom_circle(aes(x0 = 11, y0 = widthPitch / 2, r = 0.5), fill = line_col, col = line_col, lwd = 1.2) +
+    geom_circle(aes(x0 = lengthPitch - 11, y0 = widthPitch / 2, r = 0.5), fill = line_col, col = line_col, lwd = 1.2) +
     # six yard boxes
-    geom_rect(aes(xmin = 0, xmax = 5.5, ymin = (widthPitch / 2) - 9.16, ymax = (widthPitch / 2) + 9.16), fill = bg_col, col = fg_col, lwd = 1.2) +
-    geom_rect(aes(xmin = lengthPitch - 5.5, xmax = lengthPitch, ymin = (widthPitch / 2) - 9.16, ymax = (widthPitch / 2) + 9.16), fill = bg_col, col = fg_col, lwd = 1.2) +
+    geom_rect(aes(xmin = 0, xmax = 5.5, ymin = (widthPitch / 2) - 9.16, ymax = (widthPitch / 2) + 9.16), fill = bg_col, col = line_col, lwd = 1.2) +
+    geom_rect(aes(xmin = lengthPitch - 5.5, xmax = lengthPitch, ymin = (widthPitch / 2) - 9.16, ymax = (widthPitch / 2) + 9.16), fill = bg_col, col = line_col, lwd = 1.2) +
     # goals
-    geom_rect(aes(xmin = -2, xmax = 0, ymin = (widthPitch / 2) - 3.66, ymax = (widthPitch / 2) + 3.66), fill = bg_col, col = fg_col, lwd = 1.2) +
-    geom_rect(aes(xmin = lengthPitch, xmax = lengthPitch + 2, ymin = (widthPitch / 2) - 3.66, ymax = (widthPitch / 2) + 3.66), fill = bg_col, col = fg_col, lwd = 1.2) +
+    geom_rect(aes(xmin = -2, xmax = 0, ymin = (widthPitch / 2) - 3.66, ymax = (widthPitch / 2) + 3.66), fill = bg_col, col = line_col, lwd = 1.2) +
+    geom_rect(aes(xmin = lengthPitch, xmax = lengthPitch + 2, ymin = (widthPitch / 2) - 3.66, ymax = (widthPitch / 2) + 3.66), fill = bg_col, col = line_col, lwd = 1.2) +
     coord_fixed() +
     xlab("") +
     ylab("") +
--- R/soccerSpokes.R
+++ R/soccerSpokes.R
@@ -12,6 +12,9 @@ NULL
 #' @param angleBins integer, the number of angle bins movement directions are divided up into. For example, a value of 4 clusters directions in each bin into north, east, south and west.
 #' @param lengthPitch,widthPitch numeric, length and width of pitch in metres.
 #' @param grass if TRUE, draws pitch background in green and lines in white. If FALSE, draws pitch background in white and lines in black.
+#' @param line_col colour of pitch lines
+#' @param minLength numeric, ratio between size of shortest arrow and longest arrow depending on number of events.
+#' @param minAlpha numeric, minimum alpha of the arrow with the lowest number of events.
 #' @param legend if TRUE, adds legend showing relationship between arrow transparency and number of events
 #' @param plot optional, adds spokes to an existing ggplot object if provided
 #' @return a ggplot object of a heatmap on a soccer pitch.
@@ -22,14 +25,17 @@ NULL
 #' id8 <- tromso_extra %>%
 #'   dplyr::filter(id == 8) %>%
 #'   dplyr::sample_n(100)
+#' # 10x10 x,y-bins, 8 angle-bins, grass pitch
 #' soccerSpokes(id8, xBins = 5, angleBins = 8, grass = TRUE)
+#' # 5x5 x,y-bins, 16 angle-bins, blank pitch w/ grey lines
+#' soccerSpokes(id8, xBins = 5, angleBins = 16, line_col = "grey40")
 #' # draw spokes over player heatmap
 #' p <- soccerHeatmap(id8, xBins = 5)
 #' soccerSpokes(id8, xBins = 5, plot = p)
 #' 
 #' @seealso \code{\link{soccerHeatmap}} for drawing a heatmap of player position, or \code{\link{soccerSpokes}} for summarising mean direction in each pitch sector
 #' @export
-soccerSpokes <- function(df, xBins, lengthPitch = 105, widthPitch = 68, angleBins = 16, yBins = NULL, grass = FALSE, legend = TRUE, plot = NULL) {
+soccerSpokes <- function(df, xBins, lengthPitch = 105, widthPitch = 68, angleBins = 16, yBins = NULL, grass = FALSE, line_col = "black", minLength = 0.6, minAlpha = 0.4, legend = TRUE, plot = NULL) {
   
   # check value for vertical bins and match to horizontal bins if NULL
   if(is.null(yBins)) yBins <- xBins
@@ -73,15 +79,15 @@ soccerSpokes <- function(df, xBins, lengthPitch = 105, widthPitch = 68, angleBin
   df <- left_join(df, y.bin.coords, by = "y.bin")
   df <- left_join(df, angle.bin.theta, by = "angle.bin")
   
-  df$alpha <- 0.4 + (0.6 / max(df$n.angles) * df$n.angles)
-  df$radius <- (0.6 * widthPitch / (yBins+5)) + (0.4 * (df$n.angles / max(df$n.angles)) * widthPitch / (yBins+5))
+  # df$alpha <- minAlpha + ((1-minAlpha) / max(df$n.angles) * df$n.angles)
+  df$radius <- (minLength * widthPitch / (yBins+5)) + ((1-minLength) * (df$n.angles / max(df$n.angles)) * widthPitch / (yBins+5))
   
   # plot
   if(missing(plot)) {
-    p <- soccerPitchBG(lengthPitch = lengthPitch, widthPitch = widthPitch, grass = F) +
+    p <- soccerPitchBG(lengthPitch = lengthPitch, widthPitch = widthPitch, grass = grass, line_col = line_col) +
       geom_point(data = df, aes(x = x.bin.coord, y = y.bin.coord)) +
       geom_spoke(data = df, aes(x = x.bin.coord, y = y.bin.coord, angle = angle.theta, alpha = n.angles, radius = radius), arrow=arrow(length = unit(0.15,"cm"))) +
-      scale_alpha(name="Movements", range = c(0.3, 1)) +
+      scale_alpha(name="Movements", range = c(minAlpha, 1)) +
       guides(fill=FALSE)
   } else {
     p <- plot +
