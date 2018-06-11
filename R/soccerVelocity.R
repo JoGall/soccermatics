80728a1f166ebc3b4fe4acf9e85c56d2db704a3c
Joe Gallagher <j.gallagher@vtime.net>
2018-06-11 14:13:15 +0100

    added soccerVelocity() function for computing instantaneous speed and direction

---
+++ R/soccerVelocity.R
@@ -0,0 +1,24 @@
+#' @import dplyr
+NULL
+#' Compute instantaneous distance, speed and direction from x,y-coordinates
+#'
+#' @description Compute instantaneous distance moved (in metres), speed (in metres per second), and direction (in radians) between subsequent frames in a dataframe of x,y-coordinates.
+#' 
+#' @param dat = dataframe containing unnormalised x,y-coordinates `x` and `y`, time variable `t`, and identifier `id`
+#' @return a dataframe
+#' @examples
+#' # calculate distance, speed, and direction for tromso dataset
+#' soccerVelocity(tromso)
+#' @export
+soccerVelocity <- function(dat) {
+  
+  dat %>%
+    group_by(id) %>% 
+    mutate(diff = c(NA, diff(t)),
+           v = c(NA, diff(complex(real = x, imaginary = y))),
+           direction = c(diff(Arg(v)) %% (2*pi), NA) - pi,
+           dist = Mod(v),
+           speed = dist / diff) %>% 
+    select(-diff, -v)
+  
+}
