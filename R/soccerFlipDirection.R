0cba37c2244b7497a4247f2464b99ba07da1702c
Joe Gallagher <j.gallagher@vtime.net>
2018-11-12 14:03:10 +0000

    updated documentation

--- R/soccerFlipDirection.R
+++ R/soccerFlipDirection.R
@@ -8,8 +8,8 @@ NULL
 #' i.e. teams attack in the same direction all game despite changing sides at 
 #' half-time.
 #' 
-#' @param dat = dataframe containing unnormalised x,y-coordinates
-#' @param pitchLength,pitchWidth = length, width of pitch in metres
+#' @param df = dataframe containing unnormalised x,y-coordinates
+#' @param lengthPitch,widthPitch = length, width of pitch in metres
 #' @param period = name of variable containing period labels
 #' @param periodToFlip = identity of period to flip
 #' @param x,y = name of variables containing x,y-coordinates
@@ -18,13 +18,15 @@ NULL
 #' # fake period data for tromso dataset, and flip direction of '2nd half'
 #' tromso %>% 
 #'   mutate(period = if_else(t > as.POSIXct("2013-11-07 21:14:00 GMT"), 1, 2))
-#'   soccerFlipDirection(pitchLength = 120, pitchWidth = 80, periodToFlip = 2)
+#'   soccerFlipDirection(lengthPitch = 120, widthPitch = 80, periodToFlip = 2)
 #' 
 #' @export
-soccerFlipDirection <- function(dat, pitchLength = 105, pitchWidth = 68, period = "period", periodToFlip = 1, x = "x", y = "y") {
+soccerFlipDirection <- function(df, lengthPitch = 105, widthPitch = 68, teamToFlip = NULL, periodToFlip = 1, period = "period", team = "team", x = "x", y = "y") {
 
-  dat[,x] <- ifelse(dat[,period] == periodToFlip, pitchLength - dat[,x], dat[,x])
-  dat[,y] <- ifelse(dat[,period] == periodToFlip, pitchWidth - dat[,y], dat[,y])
+  if(is.null(teamToFlip)) teamToFlip <- unique(df[,team])[1]
   
-  return(dat)
+  df[,x] <- ifelse(df[,team] == teamToFlip & df[,period] %in% periodToFlip, lengthPitch - df[,x], df[,x])
+  df[,y] <- ifelse(df[,team] == teamToFlip & df[,period] %in% periodToFlip, widthPitch - df[,y], df[,y])
+  
+  return(df)
 }
