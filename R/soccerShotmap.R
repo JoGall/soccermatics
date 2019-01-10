a35efceb120b11eab99e11fa7fea19a274c0ffb4
Joe Gallagher <j.gallagher@vtime.net>
2019-01-10 16:12:14 +0000

    added optional top xG players, custom point size range

--- R/soccerShotmap.R
+++ R/soccerShotmap.R
@@ -9,6 +9,8 @@ NULL
 #' 
 #' @param lengthPitch,widthPitch length and width of pitch, in metres
 #' @param homeTeam if \code{df} contains two teams, the name of the home team to be displayed on the left hand side of the pitch. If \code{NULL}, infers home team as the team of the first event in \code{df}.
+#' @param n_players number of highest xG players to display
+#' @param size_lim minimum and maximum size of points, \code{c(min, max)}
 #' @param theme draws a \code{light}, \code{dark}, \code{grey}, or \code{grass} coloured pitch with appropriate point colours
 #' @param title,subtitle optional, adds title and subtitle to half pitch plot. Title defaults to scoreline and team identity when two teams are defined in \code{df}.
 #' @return a ggplot object
@@ -28,7 +30,7 @@ NULL
 #'                 subtitle = "vs. Argentina, World Cup 2018")
 #' 
 #' @export
-soccerShotmap <- function(df, lengthPitch = 105, widthPitch = 68, homeTeam = NULL, theme = c("light", "dark", "grey", "grass"), title = NULL, subtitle = NULL) {
+soccerShotmap <- function(df, lengthPitch = 105, widthPitch = 68, homeTeam = NULL, n_players = 0, size_lim = c(2,15), theme = c("light", "dark", "grey", "grass"), title = NULL, subtitle = NULL) {
   
   # define colours by theme
   if(theme[1] == "grass") {
@@ -51,42 +53,43 @@ soccerShotmap <- function(df, lengthPitch = 105, widthPitch = 68, homeTeam = NUL
   
   # full pitch shotmap for two teams
   if(length(unique(df$team.name)) > 1) {
-    
+
     # home team taken as first team in df if unspecified
     if(is.null(homeTeam)) homeTeam <- df$team.name[1]
     awayTeam <- unique(df$team.name)[unique(df$team.name) != homeTeam]
-    
+
     # flip x,y-coordinates of home team and factorise variables
-    df <- df %>% 
-      soccerFlipDirection(teamToFlip = homeTeam, periodToFlip = 1:2, x = "location.x", y = "location.y", team = "team.name") %>% 
+    df <- df %>%
+      soccerFlipDirection(teamToFlip = homeTeam, periodToFlip = 1:2, x = "location.x", y = "location.y", team = "team.name") %>%
       mutate(shot.outcome = as.factor(if_else(shot.outcome.name == "Goal", 1, 0)),
              penalty = as.factor(if_else(shot.type.name == "Penalty", 1, 0)),
-             team.name = factor(team.name, levels = c(homeTeam, awayTeam)))
-    
+             team.name = factor(team.name, levels = c(homeTeam, awayTeam))) %>%
+      rename(xg = shot.statsbomb_xg)
+
     # actual goals (plus own goals)
-    goals <- df %>% 
-      group_by(team.name) %>% 
-      filter(type.name == "Shot") %>% 
+    goals <- df %>%
+      group_by(team.name) %>%
+      filter(type.name == "Shot") %>%
       dplyr::summarise(g = length(shot.outcome.name[shot.outcome.name == "Goal"]) + length(type.name[type.name == "Own Goal For"]))
-    
-    # expected goals
-    xg_totals <- df %>% 
-      filter(penalty == 0) %>% 
-      group_by(team.name) %>% 
-      dplyr::summarise(xg = sum(shot.statsbomb_xg))
-    
+
     # penalties
-    pen_totals <- df %>% 
-      group_by(team.name) %>% 
-      filter(type.name == "Shot") %>% 
+    pen_totals <- df %>%
+      group_by(team.name) %>%
+      filter(type.name == "Shot") %>%
       dplyr::summarise(pen = length(shot.outcome[penalty == 1 & shot.outcome == 1]))
-    
+
     # own goals
-    og_totals <- df %>% 
-      group_by(team.name) %>% 
+    og_totals <- df %>%
+      group_by(team.name) %>%
       dplyr::summarise(og = length(type.name[type.name == "Own Goal For"]))
-    
-    # xg labels
+
+    # expected goals
+    xg_totals <- df %>%
+      filter(penalty == 0) %>%
+      group_by(team.name) %>%
+      dplyr::summarise(xg = sum(xg))
+
+    # labels
     xg1 <- sprintf("%.2f", xg_totals$xg[1])
     if(pen_totals$pen[1] > 0 & og_totals$og[1] == 0) {
       xg1 <- paste0("(+", pen_totals$pen[1], " P) ", xg1)
@@ -103,20 +106,20 @@ soccerShotmap <- function(df, lengthPitch = 105, widthPitch = 68, homeTeam = NUL
     } else if(pen_totals$pen[2] > 0 & og_totals$og[2] > 0) {
       xg2 <- paste0(xg2, " (+", pen_totals$pen[2], " P, +", og_totals$og[2], " OG) )")
     }
-    
+
     # subset shots for plotting
     df <- df %>%
-      filter(type.name == "Shot" & penalty == 0) %>% 
-      mutate(size = scales::rescale(shot.statsbomb_xg, c(1, 15), c(0, 1))) %>% 
+      filter(type.name == "Shot" & penalty == 0) %>%
+      mutate(size = scales::rescale(xg, size_lim, c(0, 1))) %>%
       arrange(as.numeric(shot.outcome), size)
-    
+
     # plot
     p <- soccerPitch(lengthPitch, widthPitch, theme = theme[1]) +
       geom_point(data = df, aes(x = location.x, y = location.y, size = size, colour = shot.outcome), alpha = 0.8) +
       scale_size_identity() +
       scale_colour_manual(name = "Outcome", breaks = c(0,1), values = c(colMiss, colGoal)) +
       guides(colour = FALSE, size = FALSE)
-    
+
     # add labels
     p <- p +
       draw_text(paste0(xg_totals$team.name[1], "  ", goals$g[1]), x = lengthPitch / 2 - 1, y = widthPitch + 5, hjust = 1, vjust = 1, size = 15, fontface = 'bold', colour = colText) +
@@ -125,48 +128,91 @@ soccerShotmap <- function(df, lengthPitch = 105, widthPitch = 68, homeTeam = NUL
       draw_text(xg1, x = lengthPitch / 2 - 1, y = widthPitch - 5, hjust = 1, vjust = 0, size = 15, colour = colText) +
       draw_text(xg2, x = lengthPitch / 2 + 1, y = widthPitch - 5, hjust = 0, vjust = 0, size = 15, colour = colText) +
       theme(plot.margin = unit(c(-0.9,-0.9,-0.7,-0.9), "cm"))
-    
-    # half pitch if one team
+
+    # top xG by player
+    if(n_players > 0) {
+      top_xgs <- df %>%
+        group_by(player.name, team.name, position.name) %>%
+        summarise(xg = sum(xg, na.rm=T)) %>%
+        ungroup() %>%
+        mutate(name = soccerShortenName(player.name)) %>%
+        arrange(-xg) %>%
+        head(n_players) %>%
+        group_by(team.name) %>%
+        mutate(rowid = 1:n()) %>%
+        ungroup() %>%
+        mutate(label = if_else(team.name == homeTeam,
+                               sprintf("%s %s", name, sprintf("%.2f", xg)),
+                               sprintf("%s %s", sprintf("%.2f", xg), name)),
+               x = if_else(team.name == homeTeam, lengthPitch/2 - 1, lengthPitch/2 + 1),
+               hjust = if_else(team.name == homeTeam, 1, 0),
+               y = widthPitch - 5 - (rowid * 2.5))
+
+      p <- p +
+        geom_text(data = top_xgs[top_xgs$team.name == homeTeam,], aes(x, y, label = label, hjust = hjust), size = 4, colour = colText) +
+        geom_text(data = top_xgs[top_xgs$team.name != homeTeam,], aes(x, y, label = label, hjust = hjust), size = 4, colour = colText)
+    }
+
+  # half pitch if one team
   } else {
     
-    df <- df %>% 
+    df <- df %>%
       mutate(shot.outcome = as.factor(if_else(shot.outcome.name == "Goal", 1, 0)),
-             penalty = as.factor(if_else(shot.type.name == "Penalty", 1, 0)))
+             penalty = as.factor(if_else(shot.type.name == "Penalty", 1, 0))) %>% 
+      rename(xg = shot.statsbomb_xg)
     
-    goals <- df %>% 
-      filter(type.name == "Shot") %>% 
+    # goals
+    goals <- df %>%
+      filter(type.name == "Shot") %>%
       dplyr::summarise(g = length(shot.outcome.name[shot.outcome.name == "Goal"]) + length(type.name[type.name == "Own Goal For"]))
     
-    # expected goals
-    xg_totals <- df %>% 
-      filter(penalty == 0) %>% 
-      dplyr::summarise(xg = sum(shot.statsbomb_xg))
-    
     # penalties
-    pen_totals <- df %>% 
-      filter(type.name == "Shot") %>% 
+    pen_totals <- df %>%
+      filter(type.name == "Shot") %>%
       dplyr::summarise(pen = length(shot.outcome[penalty == 1 & shot.outcome == 1]))
     
-    df <- df %>% 
-      filter(type.name == "Shot" & penalty == 0) %>% 
-      mutate(size = scales::rescale(shot.statsbomb_xg, c(1, 15), c(0, 1))) %>% 
+    # expected goals
+    xg <- df %>%
+      filter(penalty == 0) %>%
+      dplyr::summarise(xg = sum(xg)) %>% 
+      pull %>% 
+      sprintf("%.2f", .)
+    
+    df <- df %>%
+      filter(type.name == "Shot" & penalty == 0) %>%
+      mutate(size = scales::rescale(xg, size_lim, c(0, 1))) %>%
       arrange(as.numeric(shot.outcome), size)
     
-    p <- soccerPitchHalf(lengthPitch, widthPitch, title = title, subtitle = subtitle, theme = theme[1]) +
-      geom_point(data = df, aes(x = location.y, y = location.x, size = size, colour = shot.outcome), alpha = 0.8) +
+    p <- soccerPitchHalf(lengthPitch, widthPitch, theme = theme[1]) +
+      geom_point(data = df, aes(x = location.y, y = location.x, size = size, colour = shot.outcome), alpha = 0.7) +
       scale_size_identity() +
       scale_colour_manual(name = "Outcome", breaks = c(0,1), values = c(colMiss, colGoal)) +
       guides(colour = FALSE, size = FALSE)
     
-    xg <- sprintf("%.2f", xg_totals$xg)
-    if(pen_totals$pen > 0) {
-      xg <- paste0(xg, " (+", pen_totals$pen, " P)")
-    }
+    # top xG by player
+    if(n_players > 0) {
+      top_xgs <- df %>%
+        group_by(player.name, position.name) %>%
+        summarise(xg = sum(xg, na.rm=T)) %>%
+        ungroup() %>%
+        mutate(name = soccerShortenName(player.name)) %>%
+        arrange(-xg) %>%
+        head(n_players) %>%
+        arrange(xg) %>%
+        mutate(rowid = 1:n()) %>%
+        mutate(label = sprintf("%s %s", sprintf("%.2f", xg), name),
+        y = lengthPitch/2 + (rowid * 2.5))
     
     p <- p +
-      annotate("text", x = 2, y = lengthPitch/2 + 6, label = paste0("Goals: ", goals$g), hjust = 0, vjust = 0, size = 5, colour = colText) +
-      annotate("text", x = 2, y = lengthPitch/2 + 2, label = paste0("xG: ", xg), hjust = 0, vjust = 0, size = 5, colour = colText)
-    
+      geom_text(data = top_xgs, aes(x = 2, y, label = label, hjust = 0), size = 4, colour = colText) +
+        annotate("text", x = 2, y = lengthPitch/2 + 6 + max(top_xgs$rowid * 2.5), label = paste0("Goals: ", goals$g), hjust = 0, vjust = 0, size = 5, colour = colText) +
+        annotate("text", x = 2, y = lengthPitch/2 + 2 + max(top_xgs$rowid * 2.5), label = paste0("xG: ", xg), hjust = 0, vjust = 0, size = 5, colour = colText)
+    } else {
+      p <- p +
+        annotate("text", x = 2, y = lengthPitch/2 + 6, label = paste0("Goals: ", goals$g), hjust = 0, vjust = 0, size = 5, colour = colText) +
+        annotate("text", x = 2, y = lengthPitch/2 + 2, label = paste0("xG: ", xg), hjust = 0, vjust = 0, size = 5, colour = colText)
+    }
+  
   }
   
   return(p)
