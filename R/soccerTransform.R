5ad8f3d1ae0831709e7923a61fb6a9a7cd69e882
Joe Gallagher <joe.gallagher@L01013578.local>
2019-05-30 16:45:03 +0100

    Updated methods for transforming Opta, ChyronHego data

--- R/soccerTransform.R
+++ R/soccerTransform.R
@@ -2,77 +2,108 @@
 NULL
 #' Normalises x,y-coordinates to metres units for use with soccermatics functions
 #'
-#' @description Normalises x,y-coordinates from between any arbitrary bounds to metre units bounded by [0 < x < pitchLength, 0 < y < pitchWidth]
+#' @description Normalises x,y-coordinates from between arbitrary limits to metre units bounded by [0 < x < pitchLength, 0 < y < pitchWidth]
 #' 
-#' @param dat dataframe containing unnormalised x,y-coordinates named `x` and `y`
-#' @param xMin,xMax,yMin,yMax range of x,y-coordinates possible in the raw dataset
+#' @param dat dataframe containing arbitrary x,y-coordinates
+#' @param xMin,xMax,yMin,yMax range of possible x,y-coordinates in the raw dataframe
 #' @param pitchLength,pitchWidth length, width of pitch in metres
+#' @param method = source of data, either \code{"opta"}, \code{"statsbomb"}, \code{"tracab"}, or \code{"manual"}
+#' @param x,y variable names of x,y-coordinates. Not required when \code{method} other than \code{"manual"} is defined; defaults to \code{"x"} and \code{"y"} if manual.
 #' @return a dataframe
 #' 
 #' @examples
 #' # Three examples with true pitch dimensions (in metres):
-#' lengthPitch <- 101
+#' lengthPitch <- 105
 #' widthPitch <- 68
 #' 
-#' # Example 1. Opta-style -------------------------------------------------------
+#' # Example 1. Opta ----------------------------------------------------------
+#' 
 #' # limits = [0 < x < 100, 0 < y < 100]
-#' # centre of pitch = [50,50]
+#' opta_df <- data.frame(team_id = as.factor(c(1, 1, 1, 2, 2)),
+#'                       x = c(50.0, 41.2, 44.4, 78.6, 76.7),
+#'                       y = c(50.0, 55.8, 47.5, 55.1, 45.5),
+#'                       endx = c(42.9, 40.2, 78.0, 80.5, 72.4),
+#'                       endy = c(57.6, 47.2, 55.6, 48.1, 26.3))
 #' 
-#' opta_df <- data.frame(t = 1:12,
-#'                  x = c(50,55,61,66,62,58,51,44,45,42,41,32),
-#'                  y = c(50,48,47,40,42,45,49,51,59,75,88,100))
-#' 
-#' opta_df <- soccerTransform(opta_df, 0, 100, 0, 100, lengthPitch, widthPitch)
-#' 
-#' soccerPath(opta_df, lengthPitch = lengthPitch, widthPitch = widthPitch)
+#' soccerTransform(opta_df, method = "opta")
 #' 
 #' 
-#' # Example 2. StrataBet-style --------------------------------------------------
-#' # limits = [0 < x < 420, -136 < y < 136]
-#' # centre of pitch = [210,0]
+#' # Example 2. StatsBomb -----------------------------------------------------
 #' 
-#' stratabet_df <- data.frame(t = 1:12,
-#'                  x = c(210,222,201,192,178,170,143,122,104,91,75,60),
-#'                  y = c(0,-5,-20,-12,-8,-2,4,8,13,20,30,45))
+#' # limits = [0 < x < 120, 0 < y < 80]
 #' 
-#' stratabet_df <- soccerTransform(stratabet_df, 0, 420, -136, 136, lengthPitch, widthPitch)
-#' 
-#' soccerPath(stratabet_df, lengthPitch = lengthPitch, widthPitch = widthPitch)
+#' soccerTransform(statsbomb, method = "statsbomb")
 #' 
 #' 
-#' # Example 3. Other ------------------------------------------------------------
+#' # Example 3. TRACAB --------------------------------------------------------
+#' 
 #' # limits = [-5250 < x < 5250, -3400 < y < 3400]
-#' # centre of pitch = [0,0]
 #' 
 #' xMin <- -5250
 #' xMax <- 5250
 #' yMin <- -3400
 #' yMax <- 3400
 #' 
-#' df <- data.frame(x = c(0,-452,-982,-1099,-1586,-2088,-2422,-2999,-3200,-3857),
-#'                  y = c(0,150,300,550,820,915,750,620,400,264))
+#' tracab_df <- data.frame(x = c(0,-452,-982,-1099,-1586,-2088,-2422,-2999,-3200,-3857),
+#'                         y = c(0,150,300,550,820,915,750,620,400,264))
 #' 
-#' df <- soccerTransform(df, -5250, 5250, -3400, 3400, lengthPitch, widthPitch)
+#' soccerTransform(tracab_df, -5250, 5250, -3400, 3400, method = "tracab")
 #' 
-#' soccerPath(df, lengthPitch = lengthPitch, widthPitch = widthPitch)
+#' 
+#' # Example 4. Manual -----------------------------------------------------
+#' 
+#' # limits = [0 < x < 420, -136 < y < 136]
+#'
+#' my_df <- data.frame(team = as.factor(c(1, 1, 1, 2, 2)),
+#'                     my_x = c(210, 173, 187, 330, 322),
+#'                     my_y = c(0, 16, -7, 14, -12),
+#'                     my_endx = c(180, 169, 328, 338, 304),
+#'                     my_endy = c(21, -8, 15, -5, -65))
+#'                     
+#' soccerTransform(my_df, 0, 420, -136, 136, x = c("my_x", "my_endx"), y = c("my_y", "my_endy"))
 #' 
 #' @export
-soccerTransform <- function(dat, xMin, xMax, yMin, yMax, lengthPitch = 105, widthPitch = 68, method = c("manual", "statsbomb", "opta")) {
+soccerTransform <- function(dat, xMin, xMax, yMin, yMax, lengthPitch = 105, widthPitch = 68, method = c("manual", "statsbomb", "opta"), x = "x", y = "y") {
   
   if(method[1] == "statsbomb") {
+    
+    xMin <- 1
+    xMax <- 120
+    yMin <- 1
+    yMax <- 80
+    
     dat <- dat %>% 
-      mutate_at(vars(contains('.x')), funs((. - 1) / diff(c(1, 120)) * lengthPitch)) %>% 
-      mutate_at(vars(contains('.y')), funs((. - 1) / diff(c(1, 80)) * widthPitch))
+      mutate_at(vars(contains('.x')), funs((. - xMin) / diff(c(xMin, xMax)) * lengthPitch)) %>% 
+      mutate_at(vars(contains('.y')), funs((. - yMin) / diff(c(yMin, yMax)) * widthPitch))
     
   } else if(method[1] == "opta") {
+    
+    xMin <- 0
+    xMax <- 100
+    yMin <- 0
+    yMax <- 100
+    
     dat$x <- (dat$x - xMin) / diff(c(xMin,xMax)) * lengthPitch
     dat$y <- (dat$y - yMin) / diff(c(yMin,yMax)) * widthPitch
-    dat$endX <- (dat$endX - xMin) / diff(c(xMin,xMax)) * lengthPitch
-    dat$endY <- (dat$endY - yMin) / diff(c(yMin,yMax)) * widthPitch
+    dat$endx <- (dat$endx - xMin) / diff(c(xMin,xMax)) * lengthPitch
+    dat$endy <- (dat$endy - yMin) / diff(c(yMin,yMax)) * widthPitch
+    
+  } else if(method[1] == "tracab") {
+    
+    xMin <- -5250
+    xMax <- 5250
+    yMin <- -3400
+    yMax <- 3400
+    
+    dat$x <- (dat$x - xMin) / diff(c(xMin,xMax)) * lengthPitch
+    dat$y <- (dat$y - yMin) / diff(c(yMin,yMax)) * widthPitch
     
   } else {
-    dat$x <- (dat$x - xMin) / diff(c(xMin,xMax)) * lengthPitch
-    dat$y <- (dat$y - yMin) / diff(c(yMin,yMax)) * widthPitch
+
+    dat <- dat %>% 
+      mutate_at(vars(!!!enquo(x)), funs((. - xMin) / diff(c(xMin,xMax)) * lengthPitch)) %>% 
+      mutate_at(vars(!!!enquo(y)), funs((. - yMin) / diff(c(yMin,yMax)) * widthPitch))
+    
   }
   
   return(dat)
