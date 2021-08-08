e350d64568a24741c87cabbcfcb33b38c933455b
Joe Gallagher <joe.gallagher@statsperform.com>
2021-08-08 22:55:09 +0100

    updated docs; bind global variables; more explicit created variable names

--- R/soccerVelocity.R
+++ R/soccerVelocity.R
@@ -1,25 +1,30 @@
 #' @import dplyr
+#' @importFrom magrittr "%>%"
 NULL
 #' Compute instantaneous distance, speed and direction from x,y-coordinates
 #'
 #' @description Compute instantaneous distance moved (in metres), speed (in metres per second), and direction (in radians) between subsequent frames in a dataframe of x,y-coordinates.
 #' 
-#' @param dat dataframe containing unnormalised x,y-coordinates `x` and `y`, time variable `t`, and identifier `id`
-#' @return a dataframe
+#' @param dat dataframe containing unnormalised x,y-coordinates \code{x} and \code{y}, time variable \code{'t'}, and player identifier \code{'id'}
+#' @return a dataframe with columns \code{'dist'}, \code{'speed'}, and \code{'direction'} added
 #' @examples
-#' # calculate distance, speed, and direction for tromso dataset
+#' data(tromso)
+#' 
+#' # calculate distance, speed, and direction for \code{tromso} dataset
 #' soccerVelocity(tromso)
 #' 
 #' @export
 soccerVelocity <- function(dat) {
+  delta_t<-x<-y<-velocity<-distance<-NULL
   
   dat %>%
     group_by(id) %>% 
-    mutate(diff = c(NA, diff(t)),
-           v = c(NA, diff(complex(real = x, imaginary = y))),
-           direction = c(diff(Arg(v)) %% (2*pi), NA) - pi,
-           dist = Mod(v),
-           speed = dist / diff) %>% 
-    select(-diff, -v)
+    mutate(delta_t = c(NA, diff(t)),
+           velocity = c(NA, diff(complex(real = x, imaginary = y))),
+           direction = c(diff(Arg(velocity)) %% (2*pi), NA) - pi,
+           distance = Mod(velocity),
+           speed = distance / delta_t) %>% 
+    select(-delta_t, -velocity) %>% 
+    ungroup()
   
 }
