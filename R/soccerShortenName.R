1062e6a6df697c4156958a79ede502eb70b33684
Joe Gallagher <joe.gallagher@statsperform.com>
2021-08-08 22:53:04 +0100

    handle double prefixes of 'van de' and 'van der'

--- R/soccerShortenName.R
+++ R/soccerShortenName.R
@@ -3,10 +3,26 @@
 #' 
 #' @param names vector of strings containing full player name
 #' @examples
-#' attach(statsbomb)
+#' data(statsbomb)
 #' statsbomb$name <- soccerShortenName(statsbomb$player.name)
 #' 
 #' @export
 soccerShortenName <- function(names) {
-  sub(":", " ", sub(".* ", "", sub(" (di|De|de|El|el|Da|da|Dos|dos|Van|van|Von|von|Le|le|La|la|N') ", " \\1:", names)))
+  
+  # collapse special cases with >1 prefix separated by a space
+  names <- sub("van der", "_vander_", names)
+  names <- sub("van de", "_vande_", names)
+  
+  # define prefixes
+  prefixes <- " (Di|di|De|de|El|el|Da|da|Dos|dos|Van|van|Von|von|Le|le|La|la|N'|_vander_|_vande_) "
+  
+  # remove all of string before last name and any defined prefixes
+  names <- sub(":", " ", sub(".* ", "", sub(prefixes, " \\1:", names)))
+  
+  # expand special cases
+  names <- sub("_vander_", "van der", names)
+  names <- sub("_vande_", "van de", names)
+  
+  return(names)
+  
 }
