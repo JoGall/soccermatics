0df708c9bc8bc1a6b95061d9434cf25cd3f3c225
Joe Gallagher <joe.gallagher@statsperform.com>
2021-08-08 22:51:21 +0100

    symbol from x,y,t,id variable args; updated docs; bind global variables

--- R/soccerResample.R
+++ R/soccerResample.R
@@ -4,62 +4,64 @@
 #' @importFrom plyr rbind.fill
 #' @importFrom xts xts
 NULL
-#' Resample the frequency of x,y,t- time series with linear interpolation of x,y-coordinates.
+#' Resample the frames per second of any tracking data using linear interpolation
 #'
-#' @description Downsample or upsample dataframe containing x,y-coordinates and a time variable `t` with linear interpolation of x,y-coordinates and constant interpolation of all other variables.
+#' @description Downsample or upsample any tracking data containing x,y,t data using linear interpolation of x,y-coordinates (plus constant interpolation of all other variables in dataframe)
 #' 
-#' @param dat = dataframe containing x,y-coordinates with time variable
+#' @param df a dataframe containing x,y-coordinates and time variable
 #' @param r resampling rate in frames per second
-#' @param x,y = name of variables containing x,y-coordinates
-#' @param t = name of variable containing time data
-#' @return a dataframe
+#' @param x,y name of variables containing x,y-coordinates
+#' @param t name of variable containing time data
+#' @param id name of variable containing player identifier
+#' @return a dataframe with interpolated rows added
 #' @examples
+#' data(tromso)
+#' 
 #' # resample tromso dataset from ~21 fps to 10 fps
-#' soccerResample(tromso)
+#' soccerResample(tromso, r=10)
 #' 
 #' @export
-soccerResample <- function(dat, r = 10, x = "x", y = "y", t = "t") {
-  
-  dat$x <- dat[,x]
-  dat$y <- dat[,y]
-  dat$t <- dat[,t]
+soccerResample <- function(df, r = 10, x = "x", y = "y", t = "t", id = "id") {
+  Index<-NULL
   
   # create new time index 
-  time.index <- seq(min(dat$t), max(dat$t), by = as.difftime(1/r, units='secs'))
+  time.index <- seq(min(df[,t]), max(df[,t]), by = as.difftime(1/r, units='secs'))
   
-  # remove ALL rows that have duplicated timestamps
-  dat <- dat %>% 
-    group_by(id) %>% 
-    filter(!(duplicated(t) | duplicated(t, fromLast = TRUE))) %>% 
+  # remove all rows that have duplicated timestamps
+  df <- df %>% 
+    group_by(!!sym(id)) %>% 
+    filter(!(duplicated(!!sym(t)) | duplicated(!!sym(t), fromLast = TRUE))) %>% 
     ungroup()
   
   # resample and interpolate for each id
-  dat.proc <- lapply(unique(dat$id), function(x) {
-    
+  ids <- as.numeric(as.vector(unique(df[[id]])))
+  df_resampled <- lapply(ids, function(i) {
     #subset
-    ss <- dat[dat$id == x,]
+    ss <- df[df[,id] == i,]
     
     # convert data to xts object
-    ss.xts <- xts(ss %>% select(-t), ss$t)
+    ss.xts <- xts(ss[, names(ss) != t], ss[[t]])
     
     # join to time index
-    ss.join <- merge(ss.xts, time.index, all=T) %>% 
+    ss.join <- merge(ss.xts, time.index, all=TRUE) %>% 
       ggplot2::fortify() %>% 
       rename_at(vars(Index),~"t")
     
     # linear interpolatation of x,y-coords with omission of leading / lagging NAs; constant interpolation of other variables
     ss.join %>% 
-      mutate_at(vars(-one_of("t", "x", "y")), function(x) na.approx(x, method = "constant", na.rm=F)) %>%
-      mutate_at(vars(one_of("x", "y")), function(x) na.approx(x, na.rm=F)) %>% 
-      filter(t %in% time.index)
+      mutate_at(vars(-one_of(t, x, y)), function(x) na.approx(x, method = "constant", na.rm=FALSE)) %>%
+      mutate_at(vars(one_of(x, y)), function(x) na.approx(x, na.rm=FALSE)) %>% 
+      filter(!!sym(t) %in% time.index)
     
   }) %>% 
-    rbind.fill()
+    plyr::rbind.fill()
   
   # generate frame variable
   time.index2 <- data.frame(t = time.index, frame = 1:length(time.index))
-  dat.proc <- left_join(dat.proc, time.index2, by = "t")
+  names(time.index2)[names(time.index2) == "t"] <- t
+  df_resampled <- left_join(df_resampled, time.index2, by = t)
   
-  return(dat.proc)
+  return(df_resampled)
   
 }
+
