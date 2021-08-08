609b075f20faecd6c9d3bfd51760a8f9d472d499
Joe Gallagher <joe.gallagher@statsperform.com>
2021-08-08 22:56:41 +0100

    update deprecated non-standard evaluation and dplyr::mutate funs usage

--- R/soccerTransform.R
+++ R/soccerTransform.R
@@ -1,13 +1,15 @@
 #' @import dplyr
+#' @importFrom magrittr "%>%"
+#' @importFrom rlang enquo ":=" "!!"
 NULL
 #' Normalises x,y-coordinates to metres units for use with soccermatics functions
 #'
-#' @description Normalises x,y-coordinates from between arbitrary limits to metre units bounded by [0 < x < pitchLength, 0 < y < pitchWidth]
+#' @description Normalise x,y-coordinates from between arbitrary limits to metre units bounded by [0 < \code{"x"} < \code{"pitchLength"}, 0 < \code{"y"} < \code{"pitchWidth"}]
 #' 
-#' @param dat dataframe containing arbitrary x,y-coordinates
+#' @param df dataframe containing arbitrary x,y-coordinates
 #' @param xMin,xMax,yMin,yMax range of possible x,y-coordinates in the raw dataframe
-#' @param pitchLength,pitchWidth length, width of pitch in metres
-#' @param method = source of data, either \code{"opta"}, \code{"statsbomb"}, \code{"tracab"}, or \code{"manual"}
+#' @param lengthPitch,widthPitch length, width of pitch in metres
+#' @param method source of data, either \code{"opta"}, \code{"statsbomb"}, \code{"chyronhego"} (ChryonHego), or \code{"manual"}
 #' @param x,y variable names of x,y-coordinates. Not required when \code{method} other than \code{"manual"} is defined; defaults to \code{"x"} and \code{"y"} if manual.
 #' @return a dataframe
 #' 
@@ -31,11 +33,11 @@ NULL
 #' # Example 2. StatsBomb -----------------------------------------------------
 #' 
 #' # limits = [0 < x < 120, 0 < y < 80]
-#' 
+#' data(statsbomb)
 #' soccerTransform(statsbomb, method = "statsbomb")
 #' 
 #' 
-#' # Example 3. TRACAB --------------------------------------------------------
+#' # Example 3. ChyronHego --------------------------------------------------------
 #' 
 #' # limits = [-5250 < x < 5250, -3400 < y < 3400]
 #' 
@@ -44,10 +46,10 @@ NULL
 #' yMin <- -3400
 #' yMax <- 3400
 #' 
-#' tracab_df <- data.frame(x = c(0,-452,-982,-1099,-1586,-2088,-2422,-2999,-3200,-3857),
+#' ch_df <- data.frame(x = c(0,-452,-982,-1099,-1586,-2088,-2422,-2999,-3200,-3857),
 #'                         y = c(0,150,300,550,820,915,750,620,400,264))
 #' 
-#' soccerTransform(tracab_df, -5250, 5250, -3400, 3400, method = "tracab")
+#' soccerTransform(ch_df, -5250, 5250, -3400, 3400, method = "chyronhego")
 #' 
 #' 
 #' # Example 4. Manual -----------------------------------------------------
@@ -63,7 +65,7 @@ NULL
 #' soccerTransform(my_df, 0, 420, -136, 136, x = c("my_x", "my_endx"), y = c("my_y", "my_endy"))
 #' 
 #' @export
-soccerTransform <- function(dat, xMin, xMax, yMin, yMax, lengthPitch = 105, widthPitch = 68, method = c("manual", "statsbomb", "opta"), x = "x", y = "y") {
+soccerTransform <- function(df, xMin, xMax, yMin, yMax, lengthPitch = 105, widthPitch = 68, method = c("manual", "statsbomb", "opta", "chyronhego", "ch"), x = "x", y = "y") {
   
   if(method[1] == "statsbomb") {
     
@@ -72,9 +74,9 @@ soccerTransform <- function(dat, xMin, xMax, yMin, yMax, lengthPitch = 105, widt
     yMin <- 1
     yMax <- 80
     
-    dat <- dat %>% 
-      mutate_at(vars(contains('.x')), funs((. - xMin) / diff(c(xMin, xMax)) * lengthPitch)) %>% 
-      mutate_at(vars(contains('.y')), funs((. - yMin) / diff(c(yMin, yMax)) * widthPitch))
+    df <- df %>% 
+      mutate_at(vars(contains('.x')), list(~ (. - xMin) / diff(c(xMin, xMax)) * lengthPitch)) %>% 
+      mutate_at(vars(contains('.y')), list(~ (. - yMin) / diff(c(yMin, yMax)) * widthPitch))
     
   } else if(method[1] == "opta") {
     
@@ -83,29 +85,29 @@ soccerTransform <- function(dat, xMin, xMax, yMin, yMax, lengthPitch = 105, widt
     yMin <- 0
     yMax <- 100
     
-    dat$x <- (dat$x - xMin) / diff(c(xMin,xMax)) * lengthPitch
-    dat$y <- (dat$y - yMin) / diff(c(yMin,yMax)) * widthPitch
-    dat$endx <- (dat$endx - xMin) / diff(c(xMin,xMax)) * lengthPitch
-    dat$endy <- (dat$endy - yMin) / diff(c(yMin,yMax)) * widthPitch
+    df$x <- (df$x - xMin) / diff(c(xMin,xMax)) * lengthPitch
+    df$y <- (df$y - yMin) / diff(c(yMin,yMax)) * widthPitch
+    df$endx <- (df$endx - xMin) / diff(c(xMin,xMax)) * lengthPitch
+    df$endy <- (df$endy - yMin) / diff(c(yMin,yMax)) * widthPitch
     
-  } else if(method[1] == "tracab") {
+  } else if(method[1] %in% c("chyronhego", "ch")) {
     
     xMin <- -5250
     xMax <- 5250
     yMin <- -3400
     yMax <- 3400
     
-    dat$x <- (dat$x - xMin) / diff(c(xMin,xMax)) * lengthPitch
-    dat$y <- (dat$y - yMin) / diff(c(yMin,yMax)) * widthPitch
+    df$x <- (df$x - xMin) / diff(c(xMin,xMax)) * lengthPitch
+    df$y <- (df$y - yMin) / diff(c(yMin,yMax)) * widthPitch
     
   } else {
 
-    dat <- dat %>% 
-      mutate_at(vars(!!!enquo(x)), funs((. - xMin) / diff(c(xMin,xMax)) * lengthPitch)) %>% 
-      mutate_at(vars(!!!enquo(y)), funs((. - yMin) / diff(c(yMin,yMax)) * widthPitch))
+    df <- df %>% 
+      mutate_at(vars(!!enquo(x)), list(~ (. - xMin) / diff(c(xMin,xMax)) * lengthPitch)) %>% 
+      mutate_at(vars(!!enquo(y)), list(~ (. - yMin) / diff(c(yMin,yMax)) * widthPitch))
     
   }
   
-  return(dat)
+  return(df)
   
 }
