f24f8cf5f93c87f460bbe3dc431739dcf054839d
Joe Gallagher <joe.gallagher@statsperform.com>
2021-08-09 10:28:22 +0100

    change fun name to soccerStandardiseCols

--- R/soccerStandardiseCols.R
+++ R/soccerStandardiseCols.R
@@ -1,13 +1,13 @@
 #' @import dplyr
 #' @importFrom magrittr "%>%"
 NULL
-#' Rename columns in a dataframe for easier use with other soccermatics functions
+#' Rename columns in a dataframe for easier use with other {soccermatics} functions
 #'
 #' @description Rename columns (e.g. \code{"location.x"} -> \code{"x"}, \code{"team.name"} -> \code{"team"}, etc...) to interface directly with other soccermatics functions without having to explicitly define column names as arguments. Currently only supports Statsbomb data.
 #' 
 #' @param df a dataframe of Statsbomb event data
 #' @param method source of data; only \code{"statsbomb"} currently supported
-#' @return a dataframe with modified column names
+#' @return a dataframe with column names x, y, distance, angle, player_id, player_name, team_name, event_name
 #' @examples
 #' library(dplyr)
 #' data(statsbomb)
@@ -15,14 +15,14 @@ NULL
 #' # transform x,y-coords, standardise column names
 #' my_df <- statsbomb %>% 
 #'   soccerTransform(method = 'statsbomb') %>% 
-#'   soccerStandardCols(method = 'statsbomb')
+#'   soccerStandardiseCols(method = 'statsbomb')
 #'   
-#' # feed to other functions without defining variables
+#' # feed to other functions without defining variables,
 #' # x, y, id,distance, angle, etc...
 #' soccerHeatmap(my_df)
 #' 
 #' @export
-soccerStandardCols <- function(df, method = c("statsbomb")) {
+soccerStandardiseCols <- function(df, method = c("statsbomb")) {
   location.x<-location.y<-pass.length<-pass.angle<-player.id<-player.name<-team.name<-type.name<-NULL
   
   if(method[1] == "statsbomb") {
@@ -33,10 +33,10 @@ soccerStandardCols <- function(df, method = c("statsbomb")) {
              y = location.y,
              distance = pass.length,
              angle = pass.angle,
-             id = player.id,
-             name = player.name,
-             team = team.name,
-             event = type.name)
+             player_id = player.id,
+             player_name = player.name,
+             team_name = team.name,
+             event_name = type.name)
     
   }
   
